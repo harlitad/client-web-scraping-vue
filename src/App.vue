07324d1840e96bffed5eafcd6ff37621cc034bc5
harlita <harlitad@gmail.com>
2021-04-25 16:11:34 +0700

    create client app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,45 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container" >
+    <h5>List Articles</h5>
+    <div class="row row-cols-1 row-cols-md-4">
+      <div class="col mb-4" v-for="(article, index) in articles" :key="index">
+        <div class="card text-white bg-dark mb-3 h-100  " style="max-width: 18rem;">
+          <img :src="selectedImage" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">{{ article.title }}</h5>
+
+          </div>
+            <div class="card-footer"> 
+            <a :href="article.link" class="btn btn-light btn-block" target="_blank">Visit Website</a>
+            </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
 </style>
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      articles: [],
+      selectedImage: 'https://source.unsplash.com/user/erondu/1600x900'
+    };
+  },
+
+  async mounted() {
+    try {
+      const res = (await axios.get("http://localhost:5000/api/scrap")).data;
+      this.articles = res.data;
+    } catch (error) {
+      console.error("Error", error);
+    }
+  },
+};
+</script>
